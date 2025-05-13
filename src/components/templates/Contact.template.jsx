0caf9ce715b9252edeb95ebb06156a55d4f16435
AbdelrahmanNazim <amazing4nazem@gmail.com>
2025-05-13 23:26:42 +0300

    update

--- src/components/templates/Contact.template.jsx
+++ src/components/templates/Contact.template.jsx
@@ -53,9 +53,8 @@ const ContactTemplate = () => {
   }, [state.succeeded]);
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-white to-gray-50 dark:from-gray-900 dark:to-gray-800 px-6 py-16 pt-36">
+    <div className="min-h-screen bg-white dark:from-gray-900 dark:to-gray-800 px-6 py-16 pt-36">
       <div className="max-w-4xl mx-auto">
-        {/* Introduction Section */}
         <div className="text-center mb-8 md:mb-16">
           <h1 className="text-5xl font-bold mb-6 text-primary dark:text-white">
             Let's Connect
